74551214df34c4269751b9737482c51020580f3e
Isabelle Shehan <iws4kc@virginia.edu>
2021-10-03 13:08:28 -0400

    arrays

--- 04 - js/objects/objects.js
+++ 04 - js/objects/objects.js
@@ -1,4 +1,4 @@
-/* CONTROL STRUCTURES
+/* DATA STRUCTURES
 
 hi team – in this file we'll go over key data structures (objects and array) used in d3.js! 
 
@@ -15,7 +15,6 @@ objects are the foundation of how javascript stores and interacts with data, all
 and their properties.
 
 syntactically equivalent to a Python dictionary, js objects similarly mirror css property:value notation!
-
 */
 
 let envision_obj = {
@@ -43,6 +42,7 @@ let forge_obj = {
 }
 
 console.log(forge_obj)
+console.log('\n')
 
 /* 00 – ACTIVITY 1
 
@@ -63,10 +63,8 @@ create a nested object, either by adding or calling the object you created above
 
 // console.log the obj
 
-console.log('\n')
 
 /* 01 – ACCESSING OBJECT PROPERTIES 
-
 */
 
 // dot notation
@@ -85,10 +83,60 @@ console.log('pcs for best course?', forge_obj['best_course']['pcs'])
 console.log(Object.keys(forge_obj))
 console.log(Object.values(forge_obj))
 console.log(Object.entries(forge_obj))
-
+console.log('\n')
 
 /* 01 – ACTIVITY 1
 
-using the object you created, use accessors to console.log() a coherent sentence describing it! 
+using an object you created, use accessors to console.log() a coherent sentence describing it! 
 (try to reference some nested properties)
 */
+
+// ex. 1
+let izzy_obj = {
+    star_sign: 'sagittarius', 
+    major: 'cs', 
+    school: 'uva', 
+    fave_bar: 'coupes', 
+    is_sad: true
+}
+
+console.log('izzy is a ' + izzy_obj.star_sign + ' ' + izzy_obj.major + ' major at ' + izzy_obj.school)
+
+// your turn!
+
+
+console.log('\n')
+
+/* 02 – ARRAYS
+
+in d3, we work with ARRAYS of objects; in this section, we'll show you js syntax for accessing, indexing,
+and manipulating arrays!
+*/
+
+let global_arr = [1, 2, 3, 4, 5]
+
+// length is a super valuable property critical for iterating through the array (python = len(arr))
+// in js, we can access that with the .length property!
+console.log('len of global_arr', global_arr.length)
+
+// accessing specific array indices uses bracket notation (don't forget arrays are 0-indexed)
+console.log('first arr element', global_arr[0])
+
+// since array elements can be objects, we can access those objects' properties by chaining array indices
+// with object accessors
+
+let grit_drinks = [
+    
+    {   name:'iced dirty chai',
+        milk_type:'almond',
+        size_in_oz: 20,
+        price: 5.75},
+
+    {   name: 'cold brew',
+        milk_type: 'oat',
+        size_in_oz: 12, 
+        price: 4.50}
+]
+
+console.log('my go-to order is a ' + grit_drinks[0].size_in_oz + ' oz ' + grit_drinks[0].name + ' with ' + 
+grit_drinks[0].milk_type + ' milk for $' + grit_drinks[0].price)
