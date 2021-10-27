4e3022ffb5424519ac9a007058e14b2987c441c2
Isabelle Shehan <iws4kc@virginia.edu>
2021-10-27 12:14:54 -0400

    skel code for activity

---
+++ 06/svg/svg-data-join-practice.js
@@ -0,0 +1,9 @@
+let data = [
+    {course:'Node', type:'Technical', students:19},
+    {course:'Wireframe', type:'Creative', students:15},
+    {course:'Envision', type:'Technical', students:7},
+    {course:'Source Lite', type:'Technical', students:10},
+    {course:'Source', type:'Technical', students:10},
+    {course:'Node Lite', type:'Technical', students:8},
+    {course:'Convert', type:'Creative', students:4},
+]
